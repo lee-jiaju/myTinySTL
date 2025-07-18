816b46b0805f69b306632f05b5cd5e44a4e38a5b
lee-jiaju <2825640659@qq.com>
2025-07-18 16:15:32 +0800

    vector half realize ,the other half copied, time to take project, nervous

--- Vector.h
+++ Vector.h
@@ -11,4 +11,31 @@
 #include "UninitializedFunctions.h"
 
 
+namespace TinySTL {
+    template <class T, class Alloc = TinySTL::allocator<T>>
+    class vector {
+    private:
+        T* start_;
+        T* finish_;
+        T* endOfStorage_;
+        typedef Alloc dataAllocator;
+    public:
+        typedef T value_type;
+        typedef T* iterator;
+        typedef const T* const_iterator;
+        typedef reverse_iterator_t<T*> reverse_iterator;
+        typedef reverse_iterator_t<const T*> const_reverse_iterator;
+        typedef iterator pointer;
+        typedef T& reference;
+        typedef const T& const_reference;
+        typedef size_t size_type;
+        typedef ptrdiff_t difference_type;
+    
+    public:
+        
+
+        // Other member functions and data members...
+    };
+}
+#include "Detail\Vector.impl.h"
 #endif
